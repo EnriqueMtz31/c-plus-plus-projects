88b5a9165a67c03d6d61021d608be2a3729b7662
EnriqueMtz31 <enrique.martinez31@outlook.com>
2022-09-27 19:39:43 -0700

    Reestructura de archivos

---
+++ ejercicios-luis/make-dirs-and-files-pract5.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+MKDIR practica-5
+CD practica-5
+MKDIR ejercicio-1
+CD ejercicio-1
+TYPE NUL > ejercicio-1.c
+CD ..
+MKDIR ejercicio-2
+CD ejercicio-2
+TYPE NUL > ejercicio-2.c
+CD ../../
